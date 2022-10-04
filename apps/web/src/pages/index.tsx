8e85510e0382852168e1a0420d00d91fd62710aa
jimmy-tempest <smartwattana@gmail.com>
2022-10-04 22:17:45 +0700

    fix: hopefully fix all the pr comments

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -5,17 +5,17 @@ import Link from "next/link";
 
 export default function Landing() {
   return (
-    <div className="flex flex-col h-[calc(100vh-6rem)] justify-between content-start gap-9">
+    <div className="flex flex-col gap-9 h-[calc(100vh-6rem)] justify-between content-start">
       <div>
-        <div>
-          <h1 className="text-[10rem] font-sora -tracking-[11.75px] break-normal">
-            <span>SK </span>
-
-            <span className="text-techdev-gradient-radial font-bold">
-              Tech Dev
-            </span>
-          </h1>
-        </div>
+        <h1 className="text-[10rem] font-sora -tracking-[11.75px] break-normal">
+          <span>SK</span>
+          <wbr />
+          <span className="text-techdev-gradient-radial font-bold">
+            Tech
+            <wbr />
+            Dev
+          </span>
+        </h1>
         <div>
           <h2 className="text-5xl font-sora leading-[3.55rem] w-[60%]">
             Technology <br /> meets entertainment.
@@ -40,16 +40,16 @@ export default function Landing() {
       </div>
       <div className="flex justify-center mb-6">
         <Image
-          src="/landing graphic.svg"
+          src="/landing-graphic.svg"
           alt="TechDev Logo with videogame controller and computer chip on its side"
-          className="drop-shadow-[0px_0px_20px_rgba(233,132,98,0.5)] shadow-pulse"
+          className="drop-shadow-[0px_0px_20px_rgba(233,132,98,0.5)]"
           width={1184}
           height={400}
         />
       </div>
 
       {/* Backdrop image that cover the screen */}
-      <div className="absolute top-0 left-0 w-screen h-screen z-[-1] opacity-50">
+      <div className="fixed top-0 left-0 w-screen h-screen z-[-1] opacity-50">
         <Image
           src="/backdrop.svg"
           layout="fill"
