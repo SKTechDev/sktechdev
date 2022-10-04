b9b785e722838ff35a3ee9f669d37d4b2c8718b2
jimmy-tempest <smartwattana@gmail.com>
2022-10-04 15:35:37 +0700

    fix: techdev heading

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -8,9 +8,12 @@ export default function Landing() {
     <div className="flex flex-col h-[calc(100vh-6rem)] justify-between content-start gap-9">
       <div>
         <div>
-          <h1 className="text-9xl font-sora">
-            <span>SK</span>
-            <span className="text-techdev-gradient-radial">TechDev</span>
+          <h1 className="text-[10rem] font-sora -tracking-[11.75px] break-normal">
+            <span>SK </span>
+
+            <span className="text-techdev-gradient-radial font-bold">
+              Tech Dev
+            </span>
           </h1>
         </div>
         <div>
@@ -21,14 +24,14 @@ export default function Landing() {
             <Link href={"/contact"}>
               <a>
                 <button className="bg-light-primary text-light-on-primary py-2 px-6 rounded-full dark:bg-dark-primary dark:text-dark-on-primary cursor-pointer hover:opacity-80 transition-all duration-300">
-                  <span className=" text-sm font-sora">Join Us</span>
+                  <span className=" text-sm font-sora">Join us</span>
                 </button>
               </a>
             </Link>
             <Link href={"/about"}>
               <a>
                 <button className="text-light-primary dark:text-dark-primary py-2 px-6 rounded-full cursor-pointer border border-light-outline dark:border-dark-outline hover:opacity-80 transition-all duration-300">
-                  <span className="text-sm font-sora">Learn More</span>
+                  <span className="text-sm font-sora">Learn more</span>
                 </button>
               </a>
             </Link>
@@ -38,18 +41,19 @@ export default function Landing() {
       <div className="flex justify-center mb-6">
         <Image
           src="/landing graphic.svg"
-          alt="Landing Graphic"
+          alt="TechDev Logo with videogame controller and computer chip on its side"
           className="drop-shadow-[0px_0px_20px_rgba(233,132,98,0.5)] shadow-pulse"
           width={1184}
           height={400}
         />
       </div>
 
-      <div className="absolute top-0 left-0 w-screen h-screen z-[-1]">
+      {/* Backdrop image that cover the screen */}
+      <div className="absolute top-0 left-0 w-screen h-screen z-[-1] opacity-50">
         <Image
           src="/backdrop.svg"
           layout="fill"
-          alt="Backdrop"
+          alt="A City Backdrop"
           objectFit="cover"
           objectPosition="center"
           // width={width}
@@ -63,9 +67,7 @@ export default function Landing() {
 Landing.getLayout = function getLayout(page: ReactElement) {
   return (
     <>
-      {/* Backdrop image that cover the screen */}
       <Layout>{page}</Layout>
     </>
   );
 };
-
